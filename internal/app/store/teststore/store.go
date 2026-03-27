f3d9ce0bf91f27c243855ec8090d25b184ba1e77
terps4life <Den080604@mail.ru>
2026-03-27 15:01:43 +0300

    pushed Make

---
+++ internal/app/store/teststore/store.go
@@ -0,0 +1,27 @@
+package teststore
+
+import (
+	"github.com/Terps489/RestAPI/internal/app/model"
+	"github.com/Terps489/RestAPI/internal/app/store"
+)
+
+type Store struct {
+	userRepository *UserRepository
+}
+
+func New() *Store {
+	return &Store{}
+}
+
+func (s *Store) User() store.UserRepository {
+	if s.userRepository != nil {
+		return s.userRepository
+	}
+
+	s.userRepository = &UserRepository{
+		store: s,
+		users: make(map[string]*model.User),
+	}
+
+	return s.userRepository
+}
--- internal/app/store/teststore/userrepository_test.go
+++ internal/app/store/teststore/userrepository_test.go
@@ -1,28 +1,22 @@
-package store_test
+package teststore_test
 
 import (
 	"testing"
 
 	"github.com/Terps489/RestAPI/internal/app/model"
-	"github.com/Terps489/RestAPI/internal/app/store"
+	"github.com/Terps489/RestAPI/internal/app/store/teststore"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUserRepository_Create(t *testing.T) {
-	db, teardown := store.TestDB(t, databaseURL)
-	defer teardown("users")
-
-	s := store.New(db)
-	u, err := s.User().Create(model.TestUser(t))
-	assert.NoError(t, err)
+	s := teststore.New()
+	u := model.TestUser(t)
+	assert.NoError(t, s.User().Create(u))
 	assert.NotNil(t, u)
 }
 
 func TestUserRepository_FindByEmail(t *testing.T) {
-	db, teardown := store.TestDB(t, databaseURL)
-	defer teardown("users")
-
-	s := store.New(db)
+	s := teststore.New()
 	email := "user@example.org"
 	_, err := s.User().FindByEmail(email)
 	assert.Error(t, err)
