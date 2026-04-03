ab9d500b4bdd6eee9e50a6a238796f06a66e2fff
terps4life <Den080604@mail.ru>
2026-04-03 17:53:00 +0300

    pushed Make

--- internal/app/store/userrepository.go
+++ internal/app/store/userrepository.go
@@ -4,5 +4,6 @@ import "github.com/Terps489/RestAPI/internal/app/model"
 
 type UserRepository interface {
 	Create(*model.User) error
+	Find(int) (*model.User, error)
 	FindByEmail(string) (*model.User, error)
 }
